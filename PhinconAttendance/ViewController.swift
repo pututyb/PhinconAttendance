8e763f6d01d78d467364efba4571a8d2d4e33240
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-07-07 09:24:21 +0700

    Initial Commit

---
+++ PhinconAttendance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhinconAttendance
+//
+//  Created by Putut Yusri Bahtiar on 07/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
